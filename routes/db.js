c70480abdc24fdb1930c7f62508d77eed4763c98
Thierry Mayrand <thierry99@videotron.ca>
2023-01-06 17:49:01 +0100

    Configured mysql

--- routes/db.js
+++ routes/db.js
@@ -87,7 +87,7 @@ router.get('/user', (req, res) => {
 router.get('/movements', (req, res) => {
     
     async function getAllMovement() {
-        const result = await promiseDb.query(`SELECT * FROM movement";`)
+        const result = await promiseDb.query(`SELECT * FROM movement;`)
         console.log(result[0])
         res.status(200).json(result[0][0])
        } 
