55ff13660389896c6cf5c299b6f173dd434d1b9a
Thierry Mayrand <thierry99@videotron.ca>
2023-01-13 13:11:49 +0100

    Configured mysql

--- routes/db.js
+++ routes/db.js
@@ -562,8 +562,8 @@ router.post('/creategym', (req, res, next) => {
 router.post('/createwod', (req, res, next) => {
     const userId = `${req.body.user.toString()}`
     // const workoutType = req.body.wodType
-   // const exercices = req.body.exercices
-    console.log(userId)
+    const exercices = req.body.exercices
+    console.log(exercices)
     res.status(200)
     // CREATE WORKOUT 
 
