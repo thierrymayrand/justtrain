ccd05a17dc3815d57176e2291e6a6b70869dfc7a
Thierry Mayrand <thierry99@videotron.ca>
2023-01-13 14:03:51 +0100

    Configured mysql

--- routes/db.js
+++ routes/db.js
@@ -572,11 +572,11 @@ router.post('/createwod', (req, res, next) => {
     // CREATE EXERCICES
     async function createExercice() {
         const values = exercices.map (x => `(${x})`).join (', ');
-        //result = await promiseDb.query(`INSERT INTO exercice (rep, weight, movementId) VALUES (${values});`)
+        result = await promiseDb.query(`INSERT INTO exercice (rep, weight, movementId) VALUES (${values});`)
         
     }
 
-    // createExercice()
+     createExercice()
 
     // TAG EVERY EXERCICE TO WORKOUT 
 });
