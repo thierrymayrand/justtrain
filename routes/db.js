6fc178428685e37b3b07852d8db647f6124f0e90
Thierry Mayrand <thierry99@videotron.ca>
2023-01-18 13:39:37 +0100

    Configured mysql

--- routes/db.js
+++ routes/db.js
@@ -50,9 +50,21 @@ router.get('/userworkouts', (req, res) => {
 router.get('/myworkouts', (req, res) => {
     const userId = req.query.id
    async function userWorkouts() {
-    const result = await promiseDb.query(`select  count(*) as wodCount, workout.id as id, timeInSec, rounds, typeName from workout 
+    const result = await promiseDb.query(`select workout.id as id, timeInSec, rounds, typeName from workout 
     JOIN workouttype ON workoutTypeId = workouttype.id
     where userId = "${userId}";`)
+    console.log(result[0])
+    res.status(200).json(result[0])
+   } 
+   userWorkouts()
+   
+})
+
+router.get('/allwod', (req, res) => {
+    const userId = req.query.id
+   async function userWorkouts() {
+    const result = await promiseDb.query(`select workout.id as id, timeInSec, rounds, typeName from workout 
+    JOIN workouttype ON workoutTypeId = workouttype.id";`)
     console.log(result[0])
     res.status(200).json(result[0])
    } 
